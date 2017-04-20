b7fdd6fc05e5d0f6fedf12c21229570a2c5bc467
Joseph Mehr <Joseph Mehr>
2017-04-20 16:06:56 -0400

    A more complete version of the server.  Machine learning is yet to be added

--- Server/Server.py
+++ Server/Server.py
@@ -1,4 +1,7 @@
-from flask import Flask, request
+#Joseph Mehr
+#Back end for the Snake Spotter database
+
+from flask import Flask, request, json
 from flask_restful import Resource, Api
 from sqlalchemy import *
 from json import dumps
@@ -8,26 +11,39 @@ database = create_engine('sqlite:///SnakeSpotter.db')
 server = Flask(__name__)
 restful_api = Api(server)
 metadata = MetaData(database)
+
 #Tables
 SnakeTable = Table('Snake', metadata, autoload=True)
 SightingTable = Table('Sighting', metadata, autoload=True)
 UserTable = Table('User', metadata, autoload=True)
+
 class Snake(Resource):
     def get(self, species):
-        #Connect to database
-        #connection = database.connect()
-        #Perform query and return JSON data
-        #query = connection.execute("SELECT s.description FROM Snake s WHERE")
-        return {'description':SnakeTable.select(SnakeTable.c.species == species).execute().fetchone()[3]}
+		row = SnakeTable.select(SnakeTable.c.species == species).execute().fetchone();
+		return {'description':row[3]};
+		
+class Sighting(Resource):
+	def post(self):
+		if request.content_type != 'application/json':
+			return {'content_type':request.content_type}
+		sighting = json.loads(request.data)
+		image = sighting['image']
+		species = 'Python'  #ML stuff goes here
+		SightingTable.insert().values({'image':sighting['image'], 'time':sighting['time'],'location':sighting['location'], 'species':sighting['species'], 'observer':sighting['observer']}).execute()
+		return {'species':species}
+		
+class User(Resource):
+	def post(self):
+		if request.content_type != 'application/json':
+			return {'content_type':request.content_type}
+		body = json.loads(request.data)
+		print body['name']
+		print UserTable.insert().values({'name':body['name']}).execute()
+		return {}
 
-#class Departmental_Salary(Resource):
-#    def get(self, department_name):
-#        conn = e.connect()
-#        query = conn.execute("select * from salaries where Department='%s'"%department_name.upper())
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-#        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-#        return result
-        #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
 restful_api.add_resource(Snake, '/snake/<string:species>')
+restful_api.add_resource(Sighting, '/sighting/')
+restful_api.add_resource(User, '/user/')
+
 if __name__ == '__main__':
      server.run()
